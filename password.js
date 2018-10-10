2510b8389592e6d303b0ecf959caac16ea4f1869
PDA <PDA@Tests-MacBook-Pro-3.local>
2018-10-10 14:57:23 -0400

    updating

--- password.js
+++ password.js
@@ -1,20 +1,44 @@
-var key1, key2, key3, d, dd, n, nn, nnn;
+var key1, key2, key3, t_up, t_down, t_pause, t_start;
+var ar_l = 0;
+var c_down = 0;
+var c_up = 0;
+var t_up = 0;
+var t_down = 0;
+var ar = [];
+var ar1 = {};
 
-window.onkeydown = function(f){
-        key1 = f.keyCode;
-    dd = new Date();
-    nn = dd.getTime();
-}
-window.onkeyup = function(e) {
-    key2 = e.keyCode;
+window.addEventListener("keydown", event => {
+    c_down = c_down + 1;
+  if (ar.length == 0){
     d = new Date();
-    n = d.getTime();
-    key3 = key2;
-    nnn = n - nn;
+    t_start = d.getTime();
+  }
+  if (c_down == 1) {
+    d = new Date();
+    t_down = d.getTime();
+    if (ar.length != 0){
+        t_pause = t_down - t_up;
+      ar.push([t_pause])
+    }
+  }
+  if (!ar1.hasOwnProperty(event.keyCode)){
+    ar1[event.keyCode] = 0;
+  }
+  });                                                                                               
+  
+window.addEventListener("keyup", event => {
+    c_up = c_up + 1;
+  key1 = Object.keys(ar1);
+  if (c_up == key1.length){
+    d = new Date();
+        t_up = d.getTime();
+    if (t_up - t_start < 10000){
+        ar.push([key1, t_up - t_down]);
+      ar1 = {};
+        c_up = 0;
+      c_down = 0;
+     }
+  }
 
-    document.getElementById('test1').innerHTML = 'keyCode = ' + key1;
-    document.getElementById('test2').innerHTML = 'keyCode = ' + key2;
-    document.getElementById('time1').innerHTML = 'timepassed1 = ' + nn;
-    document.getElementById('time2').innerHTML = 'timepassed2 = ' + n;
-    document.getElementById('time3').innerHTML = 'timepassed2 = ' + nnn;
-}
+  document.getElementById('test1').innerHTML = ar; 
+});
