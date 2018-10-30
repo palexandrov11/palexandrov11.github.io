9a683abd8b043a86bd37bd5b343d145fc665a73a
PDA <PDA@Tests-MacBook-Pro-3.local>
2018-10-30 00:03:38 -0400

    updating

--- password.js
+++ password.js
@@ -50,7 +50,7 @@ xhr.open('POST', 'https://palexandrov.000webhostapp.com/index.php');
 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 xhr.send("serverData=" + serverData);*/
 
-var data = "name=philip";
+var data = "fname=jerry";
 var serverData = data.serialize()
 $.post("https://palexandrov.000webhostapp.com/index.php", serverData);
 
