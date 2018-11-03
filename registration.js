8998dd03f3bd461c916cfc54f5712cbe15fceb6e
PDA <PDA@Tests-MacBook-Pro-3.local>
2018-11-03 19:34:40 -0400

    updating

--- registration.js
+++ registration.js
@@ -1,4 +1,5 @@
 
+
 function sendData(user, step, k, d,){
       var cli = new XMLHttpRequest();
       var url = 'https://palexandrov.000webhostapp.com/index.php?' + 'user=' + String(user) + '&' + 
@@ -6,10 +7,13 @@ function sendData(user, step, k, d,){
       cli.open('POST', url, true);
       cli.send();
   }
-
-
 function checkUser(){
 	var u = document.getElementById("username").value;
 	sendData(u, 0, 0, 0);
 	document.getElementById("demo").innerHTML = "username is being verified" + u;
 }
+
+var btn = document.getElementById("btn");
+btn.addEventListener("click", checkUser);
+
+
