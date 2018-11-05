8414b29766bbaaafe7e9fed28b53c0703e1dbe95
PDA <PDA@Tests-MacBook-Pro-3.local>
2018-11-05 15:46:21 -0500

    updating

--- process.js
+++ process.js
@@ -32,57 +32,56 @@ function sendData(user, step, k, d,){
 window.addEventListener("click", event => {
   var u = document.getElementById('username').value;
   if(u.length != 0){
-    var r = sendData();
+    var r = sendData(u, 0, 0, 0);
     document.getElementById('response').innerHTML = "username is being verified" + r;
     document.getElementById('test1').innerHTML = "username" + u;
   }  
 });
 
 
-
-window.addEventListener("keydown", event => {
-    c_down = c_down + 1;
-  if (ar.length == 0){
-    d = new Date();
-    t_start = d.getTime();
-  }
-  if (c_down == 1) {
-    d = new Date();
-    t_down = d.getTime();
-    if (ar.length != 0){
-      t_pause = t_down - t_up;
-      step_d = step_d + 2;
-      ar.push([t_pause])
-      sendData("philip", step_d, '', t_pause);
+function listen(){
+  window.addEventListener("keydown", event => {
+      c_down = c_down + 1;
+    if (ar.length == 0){
+      d = new Date();
+      t_start = d.getTime();
     }
-  }
-  if (!ar1.hasOwnProperty(event.keyCode)){
-    ar1[event.keyCode] = 0;
-  }
-  });                                                                                               
-  
-window.addEventListener("keyup", event => {
-    c_up = c_up + 1;
-  key1 = Object.keys(ar1);
-  if (c_up == key1.length){
-    d = new Date();
-        t_up = d.getTime();
-    if (t_up - t_start < 10000){
-        ar.push([key1, t_up - t_down]);
-        ar1 = {};
-        step_u = step_u + 2;
-        c_up = 0;
-      c_down = 0;
-      document.getElementById('test1').innerHTML = ar;
-      sendData("philip", step_u, key1.join(""), t_up - t_down);
-     }
-    else{
-      document.getElementById('test1').innerHTML = "DONZOO";
+    if (c_down == 1) {
+      d = new Date();
+      t_down = d.getTime();
+      if (ar.length != 0){
+        t_pause = t_down - t_up;
+        step_d = step_d + 2;
+        ar.push([t_pause])
+        sendData("philip", step_d, '', t_pause);
+      }
     }
+    if (!ar1.hasOwnProperty(event.keyCode)){
+      ar1[event.keyCode] = 0;
     }
-  });
-
-
+    });                                                                                               
+    
+  window.addEventListener("keyup", event => {
+      c_up = c_up + 1;
+    key1 = Object.keys(ar1);
+    if (c_up == key1.length){
+      d = new Date();
+          t_up = d.getTime();
+      if (t_up - t_start < 10000){
+          ar.push([key1, t_up - t_down]);
+          ar1 = {};
+          step_u = step_u + 2;
+          c_up = 0;
+        c_down = 0;
+        document.getElementById('test1').innerHTML = ar;
+        sendData("philip", step_u, key1.join(""), t_up - t_down);
+       }
+      else{
+        document.getElementById('test1').innerHTML = "DONZOO";
+      }
+      }
+    });
+}
 
 
 
