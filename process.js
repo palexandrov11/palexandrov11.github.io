5068b9f5d11a77072ce5adfb50d4a3a33b2ae784
PDA <PDA@Tests-MacBook-Pro-3.local>
2018-11-05 23:19:01 -0500

    updating

--- process.js
+++ process.js
@@ -16,6 +16,12 @@ function sendData(user, step, k, d){
                   if (cli.status === 200) {
                           alert(cli.responseText);
                           document.getElementById('response').innerHTML = cli.responseText;
+                          if (cli.responseText == 0){
+                            return "0";
+                          }else {
+                            return "1";
+                          }
+
                              // OK
                          /*    var parser = new DOMParser();
                              var htmlDoc = parser.parseFromString(cli.responseText, "text/html");
@@ -62,9 +68,8 @@ function sendData(user, step, k, d){
 
 function checkUser(){
   var u = document.getElementById('username').value;
-  sendData(u, 0, 0, 0);
-  var r = document.getElementById('response').value;
-  document.getElementById('response1').innerHTML = "hello" + r;
+  var r = sendData(u, 0, 0, 0);
+  document.getElementById('response1').innerHTML = "h" + r;
   document.getElementById('command').innerHTML = "Press start and enter in your passwords (Maximum 10 seconds)";
   document.getElementById('button').style.display = 'block';
 }
