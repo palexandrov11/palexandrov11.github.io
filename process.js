8a99e33371a23729e1ff24a6130fa9af3615036d
PDA <PDA@Tests-MacBook-Pro-3.local>
2018-11-14 18:09:58 -0500

    updating

--- process.js
+++ process.js
@@ -17,7 +17,7 @@ function sendData(user, step, k, d, i){
               if (cli.readyState === 4) {
                   if (cli.status === 200) {
                           r = cli.responseText;
-                          alert(toString(r));
+                          alert(cli.responseType);
                   } else {
                              alert('failure!'+ cli.status);
                   }
@@ -53,8 +53,7 @@ function sendData(user, step, k, d, i){
 function checkUser(){
   var user = document.getElementById('username').value;
   sendData(user, 0, 0, 0, 2);
-  document.getElementById('respone').innerHTML = typeof r;
-/*  if (toString(r) === toString(0)){
+ if (toString(r) === toString(0)){
     document.getElementById('response').innerHTML = "Username " + user + " is available";
     document.getElementById('username').innerHTML = '';
     document.getElementById('command').innerHTML = "Press start and enter in your passwords (Maximum 10 seconds from first key press)";
@@ -62,7 +61,7 @@ function checkUser(){
   } else{
     document.getElementById('response').innerHTML = "Username " + user + " is taken";
     document.getElementById('username').innerHTML = '';
-  }*/
+  }
 }
 
 
