36d7d18e3cf12f9dddf0031950d37f8733ff283a
PDA <PDA@Tests-MacBook-Pro-3.local>
2018-12-01 14:28:54 -0500

    updating

--- process.js
+++ process.js
@@ -52,14 +52,14 @@ function sendData(user, step, k, d, i){
 function checkUser(){
   var user = document.getElementById('username').value;
   sendData(user, 0, 0, 0, 2);
-  document.getElementById('response').innerHTML = "Hello";
- if (r === toString("0")){
+  document.getElementById('response').innerHTML = "hello";
+ if (r === toString(0)){
    // document.getElementById('response').innerHTML = "username " + user + " is available";
     document.getElementById('username').innerHTML = '';
     document.getElementById('command').innerHTML = "Press start and enter in your passwords (Maximum 10 seconds from first key press)";
     document.getElementById('button').style.display = 'block';
   } 
- if (r === toString("1")){
+ if (r === toString(1)){
     document.getElementById('response').innerHTML = "username " + user + " is Taken" + toString(r);
     document.getElementById('username').innerHTML = '';
   }
